d30466268ce8c08262efee02508daf13589cee47
adam mcbride <adam.mcbride@gmail.com>
2016-03-07 04:02:46 -0500

    BarPhrases app
    BarPhrases app for stored array homework

---
+++ BarPhrases/BarPhrases/DetailViewController.swift
@@ -0,0 +1,71 @@
+//
+//  DetailViewController.swift
+//  barPhrases
+//
+//  Created by adam mcbride on 3/4/16.
+//  Copyright © 2016 adam mcbride. All rights reserved.
+//
+
+import UIKit
+class DetailViewController: UIViewController,UITextViewDelegate {
+     
+     @IBOutlet weak var detailDescriptionLabel: UITextView!
+     
+     
+     var detailItem: AnyObject? {
+          didSet {
+               // Update the view.
+               self.configureView()
+          }
+     }
+     
+     func configureView() {
+          // Update the user interface for the detail item.
+          if objects.count == 0 {
+               return
+          }
+          if let label = self.detailDescriptionLabel {
+               label.text = objects[currentIndex]
+               if label.text == BLANK_NOTE {
+                    label.text = ""
+               }
+          }
+          
+     }
+     
+     override func viewDidLoad() {
+          super.viewDidLoad()
+          // Do any additional setup after loading the view, typically from a nib.
+          detailViewController = self
+          detailDescriptionLabel.becomeFirstResponder()
+          detailDescriptionLabel.delegate = self
+          self.configureView()
+     }
+     
+     override func didReceiveMemoryWarning() {
+          super.didReceiveMemoryWarning()
+          // Dispose of any resources that can be recreated.
+     }
+     
+     override func viewWillDisappear(animated: Bool) {
+          super.viewWillDisappear(animated)
+          if objects.count == 0 {
+               return
+          }
+          objects[currentIndex] = detailDescriptionLabel.text
+          if detailDescriptionLabel.text == "" {
+               objects[currentIndex] = BLANK_NOTE
+          }
+          saveAndUpdate()
+     }
+     
+     func saveAndUpdate() {
+          masterView?.save()
+          masterView?.tableView.reloadData()
+     }
+     
+     func textViewDidChange(textView: UITextView) {
+          objects[currentIndex] = detailDescriptionLabel.text
+          saveAndUpdate()
+     }
+}
